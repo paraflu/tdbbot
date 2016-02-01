299d380a32b4a05ccceb6693325364e77ef2f119
andrea <a.forlin@dataservices.it>
2016-02-01 15:01:56 +0100

    corretto riferimento a bot

--- command/mention.js
+++ command/mention.js
@@ -5,12 +5,12 @@ var cn = require('chuck-norris-api');
 Mention = function() {
   this.mention = new RegExp(/@tdb_bot/i);
 
-  this.cn_joke = function(chat_id)
+  this.cn_joke = function(bot, chat_id)
   {
     var cn = require('chuck-norris-api');
     options = {}
     cn.getRandom(options).then(function (data) {
-        this.sendMessage(chat_id, data.value.joke);
+        bot.sendMessage(chat_id, data.value.joke);
     });
   }
 
@@ -19,17 +19,17 @@ Mention = function() {
     var myZap = new RegExp(/myzap/i);
     if (myZap.test(from.username))
     {
-      this.sendMessage(chat_id, sprintf("Vanculo %s!", from.username));
+      bot.sendMessage(chat_id, sprintf("Vanculo %s!", from.username));
     }
-    var offesa = new RegExp(/merda|s.ronzo|scemo|stupido|cazzo/i);
 
+    var offesa = new RegExp(/merda|s.ronzo|scemo|stupido|cazzo/i);
     if (offesa.test(message)) {
-      this.sendMessage(chat_id, sprintf("chi dice sa di esserlo @%s nànanànàaaana", from.username));
+      bot.sendMessage(chat_id, sprintf("chi dice sa di esserlo @%s nànanànàaaana", from.username));
     }
 
     var chuck = new RegExp(/\/chuck/i);
     if (chuck.test(message))
-      this.cn_joke(chat_id);
+      this.cn_joke(bot, chat_id);
 
   }
 }
